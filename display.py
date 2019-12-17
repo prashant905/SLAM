e80bcef2065c133db1c69a851a37fb925b7fd3a2
prashant kumar <prashant905@gmail.com>
2019-12-18 00:00:49 +0100

    Feature extractor and detector

---
+++ display.py
@@ -0,0 +1,24 @@
+import cv2
+import sdl2.ext
+import sdl2
+
+
+class Display(object):
+    def __init__(self,W,H):
+        sdl2.ext.init()
+        self.W = W,H
+        self.window = sdl2.ext.Window("twitchslam", size=(W, H))
+        self.window.show()
+
+    def paint(self,img):
+        events =sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                exit(0)
+        #draw
+        surf = sdl2.ext.pixels3d(self.window.get_surface())
+        surf[:,:,0:3] = img.swapaxes(0,1)
+
+        #blit
+
+        self.window.refresh()
